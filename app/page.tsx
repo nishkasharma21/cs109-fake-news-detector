f8616b954d3183888fbec6f855f1a163567bd1cf
Nishka Sharma <nishkasharma21@gmail.com>
2026-06-03 22:10:00 -0700

    Add demo video

--- app/page.tsx
+++ app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
             <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
               <iframe
                 className="absolute inset-0 w-full h-full"
-                src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+                src="https://www.youtube.com/embed/6CJIsXlKy1w"
                 title="Project Demo"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
